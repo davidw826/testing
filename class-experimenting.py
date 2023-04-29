4fff69354e359fa1a971c1f492838569003296c4
davidw826 <53101580+davidw826@users.noreply.github.com>
2023-04-29 18:43:41 -0400

    Updated from Brython Server: 4/29/2023 6:43:41 PM

--- class-experimenting.py
+++ class-experimenting.py
@@ -1,1 +1,9 @@
+names = ["John","Mike","Sarah","Joe"]
+ages = [37,54,37,15]
 
+class Person:
+    
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        print(name,age)
