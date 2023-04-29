f3dd5b0a72b703247cc8a1e24d602dd1b60e8963
davidw826 <53101580+davidw826@users.noreply.github.com>
2023-04-29 19:25:54 -0400

    Updated from Brython Server: 4/29/2023 7:25:54 PM

--- class-experimenting.py
+++ class-experimenting.py
@@ -6,6 +6,7 @@ class Person:
     def __init__(self,name,age):
         self.name = name
         self.age = age
+        self.birthday()
         
     def __str__(self):
         return self.name+' '+str(self.age)
@@ -14,18 +15,20 @@ class Person:
         return (self.age==other.age)
     
     def __hash__(self):
-        return hash((self.age))
+        return hash(self.age)
     
     def birthday(self):
         self.age += 1
 
-people = set()
+people = []#set()
 
 for i in range(4):
     new = Person(names[i],ages[i])
-    if new.name == "Sarah":
-        new.birthday()
-    people.add(new)
+    #if new.name == "Sarah":
+    #    new.birthday()
+    people.append(new)
 
 for person in people:
     print(person)
+    
+print(sum([p.age==38 for p in people]))
